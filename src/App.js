59d2130d37ed6c0ac86cbeee3280536246e3a279
telkomdev-kevin <cosmoas13@gmail.com>
2021-02-28 22:29:56 +0700

    portfolio website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import PageWrapper from './components/PageWrapper';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
+import Service from './components/Common/Services';
+import Team from './components/Common/Team';
+import Portfolio from './components/Common/Portfolio';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <PageWrapper>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/services" component={Service} />
+          <Route path="/team" component={Team} />
+          <Route path="/portfolio" component={Portfolio} />
+          Portfolio
+        </PageWrapper>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Pages/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/about.jpg';
+import Timeline from '../Common/Timeline';
+import Team from '../Common/Team';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <Header
+          title="About us"
+          subtitle="It's really a great story"
+          showButton={false}
+          image={image}
+        />
+        <Timeline />
+        <Team />
+      </div>
+    );
+  }
+}
+
+export default About;
